08267b6e08396db8f52d814c885a4d61f0ad4af0
8ii8oo <gungchi06@gmail.com>
2025-05-16 06:54:31 +0900

    설정창 & 씬 저장

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using TMPro;
 using Unity.VisualScripting;
@@ -8,7 +9,8 @@ public enum GameState
 {
     Intro,
     Playing,
-    Dead
+    Dead,
+    Option
 }
 
 public class GameManager : MonoBehaviour
@@ -17,12 +19,16 @@ public class GameManager : MonoBehaviour
     public GameState state = GameState.Intro;
     public int Live = 3;
     public float playerStartTime;
+    public static bool GameIsPaused = false;
+
 
     [Header("References")]
     public GameObject enemySpawner;
     public GameObject IntroUI;
     public GameObject overBackground;
     public testHealth PlayerScript;
+    public GameObject Setting;
+
 
     public TMP_Text scoreText;
 
@@ -36,28 +42,30 @@ public class GameManager : MonoBehaviour
 
    void Start()
 {
-    if (PlayerPrefs.GetInt("Retry", 0) == 1)
+    if (PlayerPrefs.GetInt("Retry", 0) == 1) //처음 시작 화면 & 재시작 화면
     {
         IntroUI.SetActive(false);
         enemySpawner.SetActive(true);
         scoreText.gameObject.SetActive(false);
         state = GameState.Playing;
         playerStartTime = Time.time;
+        Time.timeScale = 1f;
 
         PlayerPrefs.SetInt("Retry", 0); 
     }
-    else
+    else 
     {
         IntroUI.SetActive(true); 
     }
 }
 
-    float CalCulateScore()
+
+    float CalCulateScore() //스코어 점수 속도
     {
         return (Time.time - playerStartTime) * 20f;
     }
 
-    void SaveHighScore()
+    void SaveHighScore() //점수 저장
     {
         int score = Mathf.FloorToInt(CalCulateScore());
         int currentHighScore = PlayerPrefs.GetInt("highScore");
@@ -68,14 +76,14 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    void Update()
+    void Update() 
     {
-        if (state == GameState.Playing)
+        if (state == GameState.Playing) //플레이 중일 때
         {
-            scoreText.gameObject.SetActive(true);
+            scoreText.gameObject.SetActive(true); //점수 띄우기
             scoreText.text = "점수:" + Mathf.FloorToInt(CalCulateScore());
         }
-        if (state == GameState.Playing && Live == 0)
+        if (state == GameState.Playing && Live == 0) //피 없을 때 점수 띄우고 게임오버 화면
         {
             PlayerScript.KillPlayer();
             enemySpawner.SetActive(false);
@@ -83,36 +91,92 @@ public class GameManager : MonoBehaviour
             state = GameState.Dead;
             scoreText.text = "점수: " + GetHighScore() + "점";
         }
+
+      //esc버튼 눌렀을 때 설정 띄우기(BGM, 효과음, 계속하기, 메인화면, 게임종료)
+         if (Input.GetKeyDown(KeyCode.Escape) && state == GameState.Playing) 
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pasue();
+            }
+        }
+
     }
 
-    public void OnClickStart()
+    public void OnClickStart() //게임시작 버튼 눌렀을 때 
     {
         state = GameState.Playing;
         IntroUI.SetActive(false);
         enemySpawner.SetActive(true);
-        playerStartTime = Time.time; 
+        playerStartTime = Time.time;
     }
 
-   public void OnClickRetry()
+   public void OnClickRetry() //죽었을 때 다시하기 버튼
 {
     if (state == GameState.Dead)
     {
-        PlayerPrefs.SetInt("Retry", 1); 
+        PlayerPrefs.SetInt("Retry", 1);
         SceneManager.LoadScene("SampleScene");
     }
 }
 
-int GetHighScore()
+int GetHighScore() 
 {
     return PlayerPrefs.GetInt("highScore");
 }
 
-    public void OnClickExit()
+    public void OnClickExit() //게임종료 버튼 눌렀을 때 메인화면으로 나가기
     {
-        if (state == GameState.Dead)
+        if (state != GameState.Intro)
         {
-            
             SceneManager.LoadScene("SampleScene");
         }
     }
+
+
+public void OnClickPlay()
+{
+    if(GameIsPaused == true)
+    {
+        Resume();
+    }
+}
+    public void Pasue()
+    {
+            Setting.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+    }
+
+    void Resume()
+    {
+            Setting.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+    }
+
+    public void OnClickSetting()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && state != GameState.Dead)
+        {
+            state = GameState.Option;
+            
+        }
+    }
+
+    public void OnClickMain()
+    {
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+        PlayerPrefs.SetInt("Retry", 0);
+        SceneManager.LoadScene("SampleScene");
+        Setting.SetActive(false);
+
+    }
+
+
 }
